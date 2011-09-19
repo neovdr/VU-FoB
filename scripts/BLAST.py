1e96d7505512b0256a1234a30bdf29b5fd76b02f
Tycho Bismeijer <tychobismeijer@gmail.com>
2011-09-19 15:59:30 +0200

    Added loop to wait when the result is not ready

--- scripts/BLAST.py
+++ scripts/BLAST.py
@@ -13,7 +13,7 @@ def matching_sequences(protein_id):
 	query = fh.read() 
 	fh.close() 
         
-        #Find a comment with request id (RID) and estimated time of completion
+        # Find a comment with request id (RID) and estimated time of completion
         # (RTOE). It looks like this:
         # <!--QBlastInfoBegin
         #     RID = 7WS3FMZW01P
@@ -24,14 +24,25 @@ def matching_sequences(protein_id):
 	rid = m.groups()[0]
 	rtoe = int(m.groups()[1])
 
-        print "Waiting for: {0:d} seconds".format(rtoe)
-	sleep(rtoe+1)
+        status = ""
+        wait_time = rtoe + 1
+        while (status != "READY"): 
+            print "Waiting for: {0:d} seconds".format(wait_time)
+            sleep(wait_time)
+            baseurl = 'http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&RID='
+            url = baseurl + rid + '&FORMAT_OBJECT=Alignment&ALIGNMENT_VIEW=Tabular&FORMAT_TYPE=Text&ALIGNMENTS=5'
+            fh = urllib2.urlopen(url)
+            result = fh.read() 
+            fh.close()
 
-	baseurl = 'http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&RID='
-	url = baseurl + rid + '&FORMAT_OBJECT=Alignment&ALIGNMENT_VIEW=Tabular&FORMAT_TYPE=Text&ALIGNMENTS=5'
-	fh = urllib2.urlopen(url)
-	result = fh.read() 
-	fh.close()
+            # Find a comment with the request status. It looks like this:
+            # <!--QBlastInfoBegin
+            #     Status=WAITING
+            # QBlastInfoEnd
+            # -->
+            m = re.search("<\!--\s*QBlastInfoBegin\s*Status=(.*)", result, re.MULTILINE)
+            status = m.groups()[0]
+            wait_time = 60
 
         return result
 
