dcb8dcdd7a8b350d0d218ac8d349ed1e7be6d4a0
Tycho Bismeijer <tychobismeijer@gmail.com>
2011-09-20 13:21:10 +0200

    WIP

--- scripts/GOTerms.py
+++ scripts/GOTerms.py
@@ -1,14 +1,49 @@
 import string
 import re
+import urllib2
 
-# Open a GO file
-f = open("P62158_GO.txt", 'r')
+class GOTerm:
+    """A single GO term supported by one or more evidence"""
+    def ___init___(self, go_id, name):
+        self.go_id = go_id
+        self.name = name
+        evidence = []
 
+    def add_evidence(self, eclass, ref):
+        evidence.append (ref, eclass)
+
+class GOTerms:
+    """Holds the GO term of a protein"""
+    def ___init___(self, protein_id):
+        self.protein_id = protein_id
+        self.terms = []
+
+    def add_term(self, go_id, name, evidence_class, evidence_ref):
+        #search for term
+        for term in this.terms:
+            if term.go_id == go_id:
+                term.add_evidence(evidence_class, evidence_ref)
+                return
+        #term not found, so add it to the list
+        new_term = GoTerm(go_id, name)
+        new_term.add_evidence(evidence_class, evidence_ref)
+        self.terms.append(new_term)
+
+def getGOTerms(proteinid):
+    baseUrl = 'http://www.ebi.ac.uk/QuickGO/GAnnotation?protein='
+    url = baseUrl + proteinid + '&format=tsv'	
+    fh = urllib2.urlopen(url)
+
+    terms = GoTerms(protein_id)
+    for line in fh:
+        r = re.split("\t", line)
+        add_term(r[6], r[7], r[9], r[8])
+        aspect = r[11]
+
+
+    fh.close()
+    return terms
+
+f = open('proteins.txt', 'r')
 for line in f:
-    r = re.split("\t", line)
-    protein_id = r[1]
-    go_term = r[6]
-    go_term_s = r[7]
-    aspect = r[11]
-    evidence = r[9]
-    print aspect, go_term, evidence
+    getGOTerms(line.strip("\n"))
