0908aa7b8408507d4b6597fae3d936bffcb019d6
Vlant Iordanou <neovdr@gmail.com>
2011-09-21 08:58:13 +0200

    Added SCOP functionality to initial script!

--- scripts/initial.py
+++ scripts/initial.py
@@ -1,4 +1,4 @@
-#Create GO Term and PFAM files for proteins in text file
+#Create GO Term PFAM and SCOP files for proteins in text file
 import urllib2 
 f = open('proteins.txt', 'r') 
 
@@ -37,6 +37,25 @@ for i in range(len(ls)):
 	result = fh.read() 
 	fh.close()	 
 	output = ls[i].strip('\n') + "_Pfam.txt"  
+	o = open(output, 'w') 
+	o.write(result) 
+	o.close() 
+	
+import urllib2 
+f = open('proteins_pdb.txt', 'r') 
+
+ls = [] 
+for line in f: 
+	print line
+	ls.append(line) 
+
+for i in range(len(ls)): 
+	baseUrl = 'http://scop.mrc-lmb.cam.ac.uk/scop/search.cgi?pdb=' 
+	url = baseUrl + ls[i].strip('\n')
+	fh = urllib2.urlopen(url) 
+	result = fh.read() 
+	fh.close()	 
+	output = ls[i].strip('\n') + "_SCOP.txt"  
 	o = open(output, 'w') 
 	o.write(result) 
 	o.close() 
