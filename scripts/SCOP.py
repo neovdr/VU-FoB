21a86722c679cda870c2558b0158add7bc5b480c
Tycho Bismeijer <tychobismeijer@gmail.com>
2011-09-26 15:13:08 +0200

    Parse the family out of the SCOP data.

--- scripts/SCOP.py
+++ scripts/SCOP.py
@@ -16,6 +16,40 @@ def get_pdbs(protein_id):
             pdbs.append(fields[1].strip())
     return pdbs
 
+def get_scop_family_uniprot(protein_id):
+    """Get the SCOP family of a protein found by it's uniprot accession"""
+    pdbs = get_pdbs(protein_id)
+    f = ""
+    n = 0
+    while (f == ""):
+        if len(pdbs) < n+1:
+            print "no pdbs in scop for: " + protein_id + " (" + str(n) +\
+                " pdbs tried)"
+            return ""
+        f = get_scop_family_pdb(pdbs[n])
+        n = n + 1
+    return f
+
+def get_scop_family_pdb(pdb):
+    """Get the SCOP family of a protein found by an PDB id"""
+    baseUrl = 'http://scop.mrc-lmb.cam.ac.uk/scop/search.cgi?lev=fa&pdb='
+    url = baseUrl + pdb
+    fh = urllib2.urlopen(url)
+    result = fh.read()
+    fh.close()
+
+    m = re.search("<title>SCOP: Family: (.*)</title>", result, re.MULTILINE)
+    if m:
+        return m.groups()[0]
+
+    m = re.search("<title>SCOP: Search Results: None</title>", result, re.MULTILINE)
+    if m:
+        print "could'nt find pdb " + str(pdb) + " in SCOP"
+        return ""
+
+    print "ERROR: unknown result from SCOP"
+
 if __name__ == "__main__":
-    pdb = get_pdbs('P08046')[0]
-    print pdb
+    f = open('proteins.txt', 'r') 
+    for line in f:
+        print get_scop_family_uniprot(line.strip())
--- scripts/initial.py
+++ scripts/initial.py
@@ -13,20 +13,3 @@ for line in f:
 	o = open(output, 'w') 
 	o.write(result) 
 	o.close() 
-	
-f = open('proteins.txt', 'r') 
-for line in f:
-        pdbs = SCOP.get_pdbs(line.strip("\n"))
-        if len(pdbs) < 1:
-            print "No pdb for: " + line
-            continue
-        pdb = pdbs[0]
-	baseUrl = 'http://scop.mrc-lmb.cam.ac.uk/scop/search.cgi?lev=fa&pdb=' 
-	url = baseUrl + pdb
-	fh = urllib2.urlopen(url) 
-	result = fh.read() 
-	fh.close()	 
-	output = pdb + "_SCOP.txt"  
-	o = open(output, 'w') 
-	o.write(result) 
-	o.close() 
